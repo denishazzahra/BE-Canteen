a1de241163f13d52a435a94e4282df64c1bc43ee
Denisha Kyla Azzahra <denishazzahra@gmail.com>
2025-01-31 16:49:48 +0700

    change to tmp

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs')
 
-const uploadDirectory = path.join(__dirname, '..', 'files');
+const uploadDirectory = path.join('/tmp', 'files');
 if (!fs.existsSync(uploadDirectory)) {
   fs.mkdirSync(uploadDirectory);
 }
